c58ac9768727b6eae4890e400c9f2382d3f29a2b
VirtueSky <virtuesky.0520@gmail.com>
2025-09-24 16:52:33 +0700

    - update method sound data and editor preview

--- Module/Audio/Runtime/SoundData.cs
+++ Module/Audio/Runtime/SoundData.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using VirtueSky.Inspector;
+using VirtueSky.Misc;
 using Random = UnityEngine.Random;
 
 namespace VirtueSky.Audio
@@ -10,6 +11,14 @@ namespace VirtueSky.Audio
     [EditorIcon("scriptable_audioclip")]
     public class SoundData : ScriptableObject
     {
+        public enum GetType
+        {
+            Random,
+            Sequence
+        }
+        
+        [GUIColor(0.8f, 1.0f, 0.6f), Space(10), SerializeField, TextArea(2, 5)]
+        private string description;
         [Space] public bool loop = false;
         [Range(0f, 1f)] public float volume = 1;
 
@@ -23,19 +32,57 @@ namespace VirtueSky.Audio
 
         [ShowIf(nameof(ConditionFadeMusic), true)]
         public float fadeOutDuration = .5f;
-
+        
+        [Space] [SerializeField] private GetType getType = GetType.Random;
         [Space, SerializeField] private List<AudioClip> audioClips;
         public bool ConditionFadeMusic => isMusicFadeVolume && soundType == SoundType.Music;
+        private int sequenceIndex = 0;
         public int NumberOfAudioClips => audioClips.Count;
-
+        public List<AudioClip> AudioClips() => audioClips;
         public AudioClip GetAudioClip()
         {
             if (audioClips.Count > 0)
             {
-                return audioClips[Random.Range(0, audioClips.Count)];
+                switch (getType)
+                {
+                    case GetType.Random:
+                        return audioClips[Random.Range(0, audioClips.Count)];
+                    case GetType.Sequence:
+                        var clip = audioClips[sequenceIndex];
+                        if (sequenceIndex < audioClips.Count - 1)
+                        {
+                            sequenceIndex++;
+                        }
+                        else
+                        {
+                            sequenceIndex = 0;
+                        }
+
+                        return clip;
+                }
             }
 
             return null;
+        }
+        public void AddAudioClip(AudioClip audioClip)
+        {
+            audioClips.Add(audioClip);
+        }
+
+        public void AddAudioClips(List<AudioClip> clips)
+        {
+            audioClips.Adds(clips);
+        }
+
+        public void AddAudioClips(AudioClip[] clips)
+        {
+            audioClips.Adds(clips);
+        }
+
+        public void ClearAudioClips()
+        {
+            if (audioClips.IsNullOrEmpty()) return;
+            audioClips.Clear();
         }
     }
 
