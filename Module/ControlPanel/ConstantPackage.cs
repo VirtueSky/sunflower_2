9a097c8b9851a289f8667102e0ef6b61b860e2a2
KhoaNT2802 <khoant.cntt.ptit2802@gmail.com>
2025-10-20 18:14:37 +0700

    - update register package
    - remove playfab
    - iap 5.0.2
    - addressable 2.7.4
    - admob 10.5.0
    - firebase 13.4.0
    - ui effect 5.10.2
    - ui particle 4.11.3
    - spine 4.2

--- Module/ControlPanel/ConstantPackage.cs
+++ Module/ControlPanel/ConstantPackage.cs
@@ -4,7 +4,7 @@
     {
         public const string VersionUnityCommon = "1.6.5";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
-        public const string MaxVersionInAppPurchase = "5.0.1";
+        public const string MaxVersionInAppPurchase = "5.0.2";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
         public const string MaxVersionNewtonsoftJson = "3.2.1";
         public const string PackageNameIOS14AdvertisingSupport = "com.unity.ads.ios-support";
@@ -12,13 +12,13 @@
         public const string PackageNameMobileNotification = "com.unity.mobile.notifications";
         public const string MaxVersionMobileNotification = "2.4.1";
         public const string PackageNameAddressables = "com.unity.addressables";
-        public const string MaxVersionAddressables = "1.21.21";
+        public const string MaxVersionAddressables = "2.7.4";
         public const string PackageNameLevelPlay = "com.unity.services.levelplay";
         public const string MaxVersionLevelPlay = "9.0.0";
         public const string PackageNameAdmob = "com.google.ads.mobile";
 
         public const string VersionAdmob =
-            "https://github.com/googleads/googleads-mobile-unity.git?path=packages/com.google.ads.mobile#v10.4.2";
+            "https://github.com/googleads/googleads-mobile-unity.git?path=packages/com.google.ads.mobile#v10.5.0";
 
         #region Google Unity
 
@@ -49,41 +49,40 @@
         public const string PackageNameFirebaseApp = "com.google.firebase.app";
 
         public const string MaxVersionFirebaseApp =
-            "https://github.com/RageAgainstThePixel/com.google.firebase.app.git#13.2.0";
+            "https://github.com/RageAgainstThePixel/com.google.firebase.app.git#13.4.0";
 
         public const string PackageNameFirebaseRemoteConfig = "com.google.firebase.remote-config";
 
         public const string MaxVersionFirebaseRemoteConfig =
-            "https://github.com/RageAgainstThePixel/com.google.firebase.remote-config.git#13.2.0";
+            "https://github.com/RageAgainstThePixel/com.google.firebase.remote-config.git#13.4.0";
 
         public const string PackageNameFirebaseAnalytics = "com.google.firebase.analytics";
 
         public const string MaxVersionFirebaseAnalytics =
-            "https://github.com/RageAgainstThePixel/com.google.firebase.analytics.git#13.2.0";
+            "https://github.com/RageAgainstThePixel/com.google.firebase.analytics.git#13.4.0";
 
         public const string PackageNameFirebaseDatabase = "com.google.firebase.database";
 
         public const string MaxVersionFirebaseDatabase =
-            "https://github.com/RageAgainstThePixel/com.google.firebase.database.git#13.2.0";
+            "https://github.com/RageAgainstThePixel/com.google.firebase.database.git#13.4.0";
 
         public const string PackageNameFirebaseAuth = "com.google.firebase.auth";
 
         public const string MaxVersionFirebaseAuth =
-            "https://github.com/RageAgainstThePixel/com.google.firebase.auth.git#13.2.0";
+            "https://github.com/RageAgainstThePixel/com.google.firebase.auth.git#13.4.0";
 
         public const string PackageNameFirebaseCrashlytics = "com.google.firebase.crashlytics";
 
         public const string MaxVersionFirebaseCrashlytics =
-            "https://github.com/RageAgainstThePixel/com.google.firebase.crashlytics.git#13.2.0";
+            "https://github.com/RageAgainstThePixel/com.google.firebase.crashlytics.git#13.4.0";
 
         #endregion
 
         public const string PackageNameAdjust = "com.adjust.sdk";
         public const string MaxVersionAdjust = "https://github.com/adjust/unity_sdk.git?path=Assets/Adjust#v5.4.3";
-        public const string PackageNamePlayFab = "com.pancake.playfab";
-
-        public const string MaxVersionPlayFab =
-            "https://github.com/pancake-llc/playfab.git#2.183.231124";
+        // public const string PackageNamePlayFab = "com.pancake.playfab";
+        // public const string MaxVersionPlayFab =
+        //     "https://github.com/pancake-llc/playfab.git#2.183.231124";
 
         public const string PackageNameAppFlyer = "appsflyer-unity-plugin";
 
@@ -94,12 +93,12 @@
         public const string PackageNameCoffeeUIEffect = "com.coffee.ui-effect";
 
         public const string MaxVersionCoffeeUIEffect =
-            "https://github.com/mob-sakai/UIEffect.git?path=Packages/src#5.9.7";
+            "https://github.com/mob-sakai/UIEffect.git?path=Packages/src#5.10.2";
 
         public const string PackageNameCoffeeUIParticle = "com.coffee.ui-particle";
 
         public const string MaxVersionCoffeeUIParticle =
-            "https://github.com/mob-sakai/ParticleEffectForUGUI.git#4.11.2";
+            "https://github.com/mob-sakai/ParticleEffectForUGUI.git#4.11.3";
 
         public const string PackageNameAppleSignIn = "com.lupidan.apple-signin-unity";
 
@@ -113,12 +112,12 @@
         public const string PackageNameSpineCsharp = "com.esotericsoftware.spine.spine-csharp";
 
         public const string MaxVersionSpineCsharp =
-            "https://github.com/EsotericSoftware/spine-runtimes.git?path=spine-csharp/src#4.1";
+            "https://github.com/EsotericSoftware/spine-runtimes.git?path=spine-csharp/src#4.2";
 
         public const string PackageNameSpineUnity = "com.esotericsoftware.spine.spine-unity";
 
         public const string MaxVersionSpineUnity =
-            "https://github.com/EsotericSoftware/spine-runtimes.git?path=spine-unity/Assets/Spine#4.1";
+            "https://github.com/EsotericSoftware/spine-runtimes.git?path=spine-unity/Assets/Spine#4.2";
 
         #endregion
 
