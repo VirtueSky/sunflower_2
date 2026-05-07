a56c042e171e0efa8fce905e44e0868f0fde1452
VirtueSky <virtuesky.0520@gmail.com>
2026-05-07 11:15:50 +0700

    fix ads error
    reset max ads callback while showing
    condition load admob ad interstitial

--- Module/Advertising/Runtime/Max/MaxAdClient.cs
+++ Module/Advertising/Runtime/Max/MaxAdClient.cs
@@ -26,6 +26,7 @@ namespace VirtueSky.Ads
 
         public override void LoadInterstitial()
         {
+            if(AdSettings.MaxInterstitialAdUnit == null || AdSettings.MaxInterstitialAdUnit.IsShowing) return;
             if (!AdSettings.MaxInterstitialAdUnit.IsReady()) AdSettings.MaxInterstitialAdUnit.Load();
         }
 
@@ -33,7 +34,7 @@ namespace VirtueSky.Ads
 
         public override void LoadRewarded()
         {
-            if(AdSettings.MaxRewardAdUnit.IsShowing) return;
+            if(AdSettings.MaxRewardAdUnit == null || AdSettings.MaxRewardAdUnit.IsShowing) return;
             if (!AdSettings.MaxRewardAdUnit.IsReady()) AdSettings.MaxRewardAdUnit.Load();
         }
 
