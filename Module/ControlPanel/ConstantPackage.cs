7318a5a9d2147457776dee9057c1b2e9591f16fa
VirtueSky <virtuesky.0520@gmail.com>
2026-06-24 00:55:25 +0700

    update 1.9.7-preview

--- Module/ControlPanel/ConstantPackage.cs
+++ Module/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionUnityCommon = "1.8.6";
+        public const string VersionUnityCommon = "1.8.7-preview";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "5.0.2";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
